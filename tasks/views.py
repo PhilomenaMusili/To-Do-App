31f932ea2c2f8f77282467b5ee395acb0e41eb2d
PhilomenaMusili <philomenamusili2@gmail.com>
2024-03-08 12:58:06 +0300

    modified:   db.sqlite3 Created item

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm  
 
 def index(request):
     tasks = Task.objects.all()
+
     form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/') 
+
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
