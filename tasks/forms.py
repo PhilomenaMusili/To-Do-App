78adec89cd6fd06858d139e0d1bc2cc86d34aba2
PhilomenaMusili <philomenamusili2@gmail.com>
2024-03-13 15:25:40 +0300

    Styled Template

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,9 @@
-from django.forms import ModelForm  
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm): 
+class TaskForm(forms.ModelForm): 
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task...'}))
+
     class Meta:
         model = Task
         fields = '__all__'
